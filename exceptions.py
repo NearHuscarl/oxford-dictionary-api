fa7eaa749c2b5c8efe24188003953a3aaaeeac2c
Near <16520846@gm.uit.edu.vn>
2018-03-22 18:05:08 +0700

    delete old file

--- exceptions.py
+++
@@ -1,8 +0,0 @@
-#!/bin/env python
-
-""" Internet connection related exceptions """
-
-# pylint: disable=unused-import
-# pylint: disable=redefined-builtin
-from requests import ConnectionError
-from requests.exceptions import HTTPError, Timeout
